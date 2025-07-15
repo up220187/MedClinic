9eaca1489fbc9bfaabaef55e7b31ca7404ceace7
ReynaMartinez <up220187@alumnos.upa.edu.mx>
2025-07-14 19:47:35 -0600

    backend init

---
+++ backend/src/main/java/com/consulta/backend/ConsultaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.consulta.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/consulta/backend/ConsultaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulta.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
